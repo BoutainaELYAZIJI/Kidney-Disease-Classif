a07e4f6822c67c65ddc4998300f891b7467894cf
Boutaina ELYAZIJI <boutaina.mumu11@gmail.com>
2023-11-07 01:16:37 +0100

    Logging and units modules added

---
+++ main.py
@@ -0,0 +1,19 @@
+from src.cnnClassifier import logger
+"""rom src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+#logger.info("Welcome to our custom log")
+
+
+STAGE_NAME="Data Ingestion"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+"""
+logger.info("Welcome to our custom log")
